1d489791bdb29deadf695e15edc825d5d3d7e577
Chris Vine <cvine@users.sourceforge.net>
2017-06-21 18:49:21 +0100

    Improve handling of parallel compilation and of Mac OS compilation

--- a-sync/a-sync-compile.sh
+++ a-sync/a-sync-compile.sh
@@ -4,4 +4,7 @@ LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
 # cater for Mac OS
 DYLD_LIBRARY_PATH=../lib:$DYLD_LIBRARY_PATH
 export LD_LIBRARY_PATH DYLD_LIBRARY_PATH
-guild compile -L .. -o $1 $2
+# call up guile directly rather than via the guild shell script: with
+# luck, with Mac OS the guile binary will be in a non-protected
+# directory
+guile --no-auto-compile -c "(let () (add-to-load-path \"..\")(compile-file \"$1\" #:output-file \"$2\"))"
