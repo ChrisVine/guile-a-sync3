e6f579f1c9cdb7935217d2844301a1f1163d67e4
Chris Vine <cvine@users.sourceforge.net>
2017-06-21 11:30:32 +0100

    Make additional provision for Mac OS X

--- a-sync/a-sync-compile.sh
+++ a-sync/a-sync-compile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
+# cater for Mac OS
+DYLD_LIBRARY_PATH=../lib:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH DYLD_LIBRARY_PATH
 guild compile -L .. -o $1 $2
