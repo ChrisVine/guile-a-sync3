94a87ac6423bab35782a6365f893a89101b72de9
Chris Vine <vine.chris@gmail.com>
2018-07-25 16:21:53 +0100

    Adjust comments

--- a-sync/event-loop.scm
+++ a-sync/event-loop.scm
@@ -172,8 +172,8 @@
        ;; thread is running in the program and it might exec
        ;; concurrently with the creation of the event loop in this
        ;; thread, but that doesn't really matter - having a child
-       ;; process sharing the descriptor for an unnamed pipe it has no
-       ;; access to isn't really a problem in such cases.
+       ;; process sharing the descriptor for an unnamed pipe which
+       ;; contains no meaningful information isn't really a problem.
        (fcntl in F_SETFD (logior FD_CLOEXEC
 				 (fcntl in F_GETFD)))
        (fcntl out F_SETFD (logior FD_CLOEXEC
@@ -530,8 +530,8 @@
       ;; thread is running in the program and it might exec
       ;; concurrently with the resetting of the event loop in this
       ;; thread, but that doesn't really matter - having a child
-      ;; process sharing the descriptor for an unnamed pipe it has no
-      ;; access to isn't really a problem in such cases.
+      ;; process sharing the descriptor for an unnamed pipe which
+      ;; contains no meaningful information isn't really a problem.
       (fcntl in F_SETFD (logior FD_CLOEXEC
 				(fcntl in F_GETFD)))
       (fcntl out F_SETFD (logior FD_CLOEXEC
