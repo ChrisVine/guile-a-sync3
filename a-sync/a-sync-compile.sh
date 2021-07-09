07c28c8ae13e98c46d66670b2aafce14b8b56580
Chris Vine <vine.chris@gmail.com>
2021-07-09 17:34:33 +0100

    Add lib/.libs to LD_LIBRARY_PATH and DYLD_LIBRARY_PATH when compiling bytecode

--- a-sync/a-sync-compile.sh
+++ a-sync/a-sync-compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=../lib:../lib/.libs:$LD_LIBRARY_PATH
 # cater for Mac OS
-DYLD_LIBRARY_PATH=../lib:$DYLD_LIBRARY_PATH
+DYLD_LIBRARY_PATH=../lib:../lib/.libs:$DYLD_LIBRARY_PATH
 export LD_LIBRARY_PATH DYLD_LIBRARY_PATH
 # call up guile directly rather than via the guild shell script: with
 # luck, with Mac OS the guile binary will be in a non-protected
